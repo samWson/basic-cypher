90602dc0b4df8cdf48616935098d9b8419bbef0b
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-23 21:34:49 +1200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to basic-cypher
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/basic_cypher/core_test.clj
@@ -0,0 +1,7 @@
+(ns basic-cypher.core-test
+  (:require [clojure.test :refer :all]
+            [basic-cypher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
