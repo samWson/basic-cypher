532aa4a0bc6d2494537900f9a30ba949badea9fe
Samuel <samWson@users.noreply.github.com>
2017-04-23 21:40:38 +1200

    Edit description, url, and license attributes
    Changed attributes from defaults.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject basic-cypher "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A simple text cypher program"
+  :url "https://github.com/samWson/basic-cypher"
+  :license {:name "MIT License"
+            :url "https://mitlicense.org/"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot basic-cypher.core
   :target-path "target/%s"
