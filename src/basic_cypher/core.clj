49281ae7f118d6ed55722b89a4cc2f2413d26dc8
Samuel Wilson <samWson@users.noreply.github.com>
2017-04-26 21:55:51 +1200

    Add get-name function
    get-name returns a string from stdin. It's intended to be used for
    getting a file name.

--- src/basic_cypher/core.clj
+++ src/basic_cypher/core.clj
@@ -2,7 +2,15 @@
   (:require [basic-cypher.tabula-recta :as tr])
   (:gen-class))
 
+(defn get-name
+  "Return a string from stdin."
+  []
+  (let [name (read-line)]
+    name))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& args])
+
+
+
